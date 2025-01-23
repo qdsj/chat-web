bfeedb63da4d31ea6c7549a6928f1f51055ef6bd
zenoskongfu <zenos.chen@foxmail.com>
2025-01-23 10:26:36 +0800

    add element-plus

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 	<div>
 		<H1>Hi, Blue</H1>
 		<H3>This is Chat Web Home</H3>
+		<el-button>Hello</el-button>
 	</div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: "/chatweb",
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		AutoImport({
+			imports: ["vue"],
+			resolvers: [ElementPlusResolver()],
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()],
+		}),
+	],
 });
