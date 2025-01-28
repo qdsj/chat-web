981ec4619f3bfc51bc1d64a971935e5b31b5cf20
zenoskongfu <zenos.chen@foxmail.com>
2025-01-28 10:03:44 +0800

    update proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,13 @@ export default defineConfig({
 			"@": resolve(__dirname, "src"),
 		},
 	},
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3200",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 });
