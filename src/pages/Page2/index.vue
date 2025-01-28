0f9ae2d83413d9ecefbba3de9af010836022bfda
zenoskongfu <zenos.chen@foxmail.com>
2025-01-28 09:58:25 +0800

    add websocket test code

--- src/pages/Page2/index.vue
+++ src/pages/Page2/index.vue
@@ -1,4 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { io } from "socket.io-client";
+
+onMounted(() => {
+	const client = io("http://localhost:3210");
+
+	client.on("connect", () => {
+		console.log("连接成功");
+
+		client.emit("test", "hello", (data: any) => {
+			console.log(data);
+		});
+
+		client.emit("connect-server", {
+			id: client.id,
+			name: "zhangsan",
+		});
+
+		client.emit("join-room", {
+			id: client.id,
+			roomId: "1",
+			name: "zhangsan",
+		});
+
+		client.emit("findAllOnline", (data: any) => {
+			console.log(data);
+		});
+	});
+
+	client.on("connect_error", () => {
+		console.log("error");
+	});
+
+	client.on("data", (data) => {
+		console.log("data: ", data);
+	});
+});
+</script>
 
 <template>
 	<div>page2</div>
