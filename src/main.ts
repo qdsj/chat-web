f0974db7942a742d6a069182760e2783ff1d026c
ljh <ljh@163.com>
2025-02-20 22:24:19 +0800

    Add Contact Management Framework

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import "element-plus/dist/index.css";
+
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount("#app");
+import "./assets/css/common.css";
+import "@/assets/icon/iconfont.css";
+import Layout from "@/components/Layout.vue";
+
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount("#app");
+app.component("Layout", Layout);
