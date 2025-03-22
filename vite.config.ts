eb37a3afcd0be16ce4c7a7d9c8901490b393eb86
zenos <zenos.chen@foxmail.com>
2025-03-22 12:48:37 +0800

    增加运行时编译的能力

--- vite.config.ts
+++ vite.config.ts
@@ -7,32 +7,33 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/chatweb",
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: ["vue"],
-      resolvers: [ElementPlusResolver()],
-      dts: "src/auto-imports.d.ts",
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-      dts: "src/components.d.ts",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:3200",
-        target: "https://dev.qdsj.top/server/chatweb",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+	base: "/chatweb",
+	plugins: [
+		vue(),
+		AutoImport({
+			imports: ["vue"],
+			resolvers: [ElementPlusResolver()],
+			dts: "src/auto-imports.d.ts",
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()],
+			dts: "src/components.d.ts",
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+			vue: "vue/dist/vue.esm-bundler.js", // 关键配置
+		},
+	},
+	server: {
+		proxy: {
+			"/api": {
+				// target: "http://localhost:3200",
+				target: "https://dev.qdsj.top/server/chatweb",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 });
