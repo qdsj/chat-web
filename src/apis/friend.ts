ed19101c10e28a85bdd9836b9f4f03c7f16a4597
zenos <zenos.chen@foxmail.com>
2025-03-22 15:28:40 +0800

    添加friend的类型标注

--- src/apis/friend.ts
+++ src/apis/friend.ts
@@ -1,44 +1,34 @@
 import { getFetch, postFetch } from "./http.ts";
+import {
+	I_AddFriendApiResult,
+	I_AgreeFriendApiResult,
+	I_FindUserByNameApiResult,
+	I_GetFriendListApiResult,
+	I_GetRequestListApiResult,
+} from "./types/friend.types.ts";
 
-export interface I_FindUserByNameApiResult {
-	data: {
-		id: "string";
-		username: "string";
-		email: "string";
-		friendShip: {
-			id: "string";
-			userId: "string";
-			friendId: "string";
-			requesterId: "string";
-			requestMessage: "string";
-			status: "string";
-			blockerId: "string";
-			createdAt: "string";
-		};
-	};
-}
 // 通过名字查找用户
 export const findUserByNameApi = (username: string): Promise<I_FindUserByNameApiResult> => {
 	return getFetch(`/user/findUserByName?username=${username}`);
 };
 
 // 添加好友
-export const addFriendApi = (data: { friendId: string; requestMessage: string }) => {
+export const addFriendApi = (data: { friendId: string; requestMessage: string }): Promise<I_AddFriendApiResult> => {
 	return postFetch("/user/addFriend", data);
 };
 
 // 获取请求列表
-export const getRequestList = () => {
+export const getRequestList = (): Promise<I_GetRequestListApiResult> => {
 	return getFetch("/user/getRequestList");
 };
 
 // 同意好友申请
-export const agreeFriend = (data: Object) => {
+export const agreeFriend = (data: Object): Promise<I_AgreeFriendApiResult> => {
 	return postFetch("/user/agreeFriend", data);
 };
 
 // 获取好友列表
-export const getFriendList = () => {
+export const getFriendList = (): Promise<I_GetFriendListApiResult> => {
 	return getFetch("/user/getFriendList");
 };
 
