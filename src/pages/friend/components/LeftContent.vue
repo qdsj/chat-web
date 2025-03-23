bb9d2f9117ef0b8b3407d6cbb0f0352339185cd1
ljh <ljh@163.com>
2025-03-23 09:17:55 +0800

    update code

--- src/pages/friend/components/LeftContent.vue
+++ src/pages/friend/components/LeftContent.vue
@@ -5,97 +5,106 @@ import ListGroup from "./ListGroup.vue";
 const friendStore = useFriendStore();
 const searchKey = ref("");
 const defaultAvatar =
-	"https://th.bing.com/th/id/OIP.-I18fiU0rmHiiS5FtMDyHgHaHa?w=207&h=207&c=7&r=0&o=5&dpr=1.5&pid=1.7";
+  "https://th.bing.com/th/id/OIP.-I18fiU0rmHiiS5FtMDyHgHaHa?w=207&h=207&c=7&r=0&o=5&dpr=1.5&pid=1.7";
 // 搜索好友 or 群聊
 const search = () => {
-	console.log(searchKey.value);
+  console.log(searchKey.value);
 };
 
 const newFriendGroup = {
-	name: "新朋友",
-	children: [
-		{
-			name: "搜好友",
-			path: "/contact/search",
-			type: "search",
-		},
-		{
-			name: "新的朋友",
-			path: "/contact/contactNotice",
-			type: "newFriend",
-		},
-	],
+  name: "新朋友",
+  children: [
+    {
+      name: "添加好友",
+      path: "/contact/search",
+      type: "search",
+    },
+    {
+      name: "新的朋友",
+      path: "/contact/contactNotice",
+      type: "newFriend",
+    },
+  ],
 };
 
 const newGroupGroup = {
-	name: "群聊",
-	children: [
-		{
-			name: "新建群聊",
-			path: "/contact/createGroup",
-			type: "group",
-		},
-		{
-			name: "相亲相爱一家人",
-			path: "/contact/groupDetail?id=group@qq.com&name=相亲相爱一家人",
-			type: "friend",
-			avatar: defaultAvatar,
-			data: {
-				email: "group@qq.com",
-				name: "相亲相爱一家人",
-			},
-		},
-	],
+  name: "群聊",
+  children: [
+    {
+      name: "新建群聊",
+      path: "/contact/createGroup",
+      type: "group",
+    },
+    {
+      name: "相亲相爱一家人",
+      path: "/contact/groupDetail?id=group@qq.com&name=相亲相爱一家人",
+      type: "friend",
+      avatar: defaultAvatar,
+      data: {
+        email: "group@qq.com",
+        name: "相亲相爱一家人",
+      },
+    },
+  ],
 };
 
 const friendGroup = {
-	name: "我的好友",
-	children: [
-		...(friendStore.friendList || []).map((item) => {
-			return {
-				name: item.username,
-				path: `/contact/userDetail?id=${item.id}&username=${item.username}`,
-				type: "friend",
-				avatar: defaultAvatar,
-				data: {
-					email: item.email,
-					name: item.username,
-				},
-			};
-		}),
-	],
+  name: "我的好友",
+  children: [
+    ...(friendStore.friendList || []).map((item) => {
+      return {
+        name: item.username,
+        path: `/contact/userDetail?id=${item.id}&username=${item.username}`,
+        type: "friend",
+        avatar: defaultAvatar,
+        data: {
+          email: item.email,
+          name: item.username,
+        },
+      };
+    }),
+  ],
 };
 </script>
 
 <template>
-	<!-- 搜索框 -->
-	<div class="top-search">
-		<el-input clearable placeholder="搜索" v-model="searchKey" size="large" @keyup="search">
-			<template #suffix>
-				<span class="iconfont icon-search"></span>
-			</template>
-		</el-input>
-	</div>
-	<!-- 联系人 or 群聊 -->
-	<div class="list-group-container">
-		<ListGroup :name="newFriendGroup.name" :children="newFriendGroup.children" />
-		<ListGroup :name="newGroupGroup.name" :children="newGroupGroup.children" />
-		<ListGroup :name="friendGroup.name" :children="friendGroup.children" />
-	</div>
+  <!-- 搜索框 -->
+  <div class="top-search">
+    <el-input
+      clearable
+      placeholder="搜索"
+      v-model="searchKey"
+      size="large"
+      @keyup="search"
+    >
+      <template #suffix>
+        <span class="iconfont icon-search"></span>
+      </template>
+    </el-input>
+  </div>
+  <!-- 联系人 or 群聊 -->
+  <div class="list-group-container">
+    <ListGroup
+      :name="newFriendGroup.name"
+      :children="newFriendGroup.children"
+    />
+    <ListGroup :name="newGroupGroup.name" :children="newGroupGroup.children" />
+    <ListGroup :name="friendGroup.name" :children="friendGroup.children" />
+  </div>
 </template>
 
 <style scoped lang="scss">
 .top-search {
-	padding: 20px 10px;
-	background-color: #f7f7f7;
-	display: flex;
-	align-items: center;
-	.iconfont {
-		font-size: 18px;
-	}
+  padding: 20px 10px;
+  background-color: #f7f7f7;
+  display: flex;
+  align-items: center;
+  .iconfont {
+    font-size: 18px;
+  }
 }
 
 .list-group-container {
-	height: calc(100vh - 92px);
+  height: calc(100vh - 92px);
 }
 </style>
--- src/store/useFriendStore.ts
+++ src/store/useFriendStore.ts
@@ -1,91 +1,103 @@
-import { addFriendApi, agreeFriendApi, findUserByNameApi, getFriendListApi, getRequestListApi } from "@/apis/friend";
-import { I_FindUserByNameApiResult, I_GetRequestListApiResult } from "@/apis/types/friend.types";
+import {
+  addFriendApi,
+  agreeFriendApi,
+  findUserByNameApi,
+  getFriendListApi,
+  getRequestListApi,
+} from "@/apis/friend";
+import {
+  I_FindUserByNameApiResult,
+  I_GetRequestListApiResult,
+} from "@/apis/types/friend.types";
 import { T_User } from "@/types/model/friend.types";
 import { ElMessage } from "element-plus";
 import { defineStore } from "pinia";
-import { useChatStore } from "./useChatStore";
 // 具体和api交互的代码，统一放在store中。
 // vue文件只需要考虑和store进行交互即可
 
 export const useFriendStore = defineStore(
-	"use-friend-store",
-	() => {
-		const chatStore = useChatStore();
-		const friendList = ref<T_User[]>([]);
+  "use-friend-store",
+  () => {
+    const friendList = ref<T_User[]>([]);
 
-		const getAllFriend = async () => {
-			const list = await getFriendListApi();
-			friendList.value = list.data;
-		};
+    const getAllFriend = async () => {
+      const list = await getFriendListApi();
+      friendList.value = list.data;
+    };
 
-		// 添加新的好友
-		const addFriend = async (friendId: string, requestMessage: string): Promise<[string | null, boolean]> => {
-			try {
-				await addFriendApi({
-					friendId,
-					requestMessage,
-				});
-				return [null, true];
-			} catch (error) {
-				return [error, false] as any;
-			}
-		};
+    // 添加新的好友
+    const addFriend = async (
+      friendId: string,
+      requestMessage: string
+    ): Promise<[string | null, boolean]> => {
+      try {
+        await addFriendApi({
+          friendId,
+          requestMessage,
+        });
+        return [null, true];
+      } catch (error) {
+        return [error, false] as any;
+      }
+    };
 
-		// 搜索好友
-		const searchUserByName = async (
-			username: string
-		): Promise<[string | null, I_FindUserByNameApiResult["data"]]> => {
-			try {
-				const res = await findUserByNameApi(username);
+    // 搜索好友
+    const searchUserByName = async (
+      username: string
+    ): Promise<[string | null, I_FindUserByNameApiResult["data"]]> => {
+      try {
+        const res = await findUserByNameApi(username);
 
-				// []表示返回两个值，第一个是error，第二个是data
-				return [null, res.data] as any;
-			} catch (error) {
-				return [error, null] as any;
-			}
-		};
+        // []表示返回两个值，第一个是error，第二个是data
+        return [null, res.data] as any;
+      } catch (error) {
+        return [error, null] as any;
+      }
+    };
 
-		// 获取请求列表
-		const getRequestList = async (): Promise<[string | null, I_GetRequestListApiResult["data"]]> => {
-			try {
-				const res = await getRequestListApi();
-				return [null, res.data] as any;
-			} catch (error) {
-				ElMessage.warning(error || "获取好友申请列表失败");
-				return [error, null] as any;
-			}
-		};
+    // 获取请求列表
+    const getRequestList = async (): Promise<
+      [string | null, I_GetRequestListApiResult["data"]]
+    > => {
+      try {
+        const res = await getRequestListApi();
+        return [null, res.data] as any;
+      } catch (error) {
+        ElMessage.warning(error || "获取好友申请列表失败");
+        return [error, null] as any;
+      }
+    };
 
-		// 同意好友请求
-		const agreeFriend = async (friendId: string) => {
-			try {
-				await agreeFriendApi({ friendId });
-				// 将好友加进会话列表
-				// chatStore.addConversation({})
-				// 跳转至会话列表，开始聊天
-				// router.push("/chat");
-			} catch (error) {
-				ElMessage.warning(error || "获取好友申请列表失败");
-			}
-		};
+    // 同意好友请求
+    const agreeFriend = async (friendId: string) => {
+      try {
+        await agreeFriendApi({ friendId });
+        // 将好友加进会话列表
+        // chatStore.addConversation({})
+        // 跳转至会话列表，开始聊天
+        // router.push("/chat");
+      } catch (error) {
+        ElMessage.warning(error || "获取好友申请列表失败");
+      }
+    };
 
-		const getFriendById = (id: string) => {
-			return friendList.value.find((friend) => friend.id === id);
-		};
+    const getFriendById = (id: string) => {
+      return friendList.value.find((friend) => friend.id === id);
+    };
 
-		return {
-			friendList,
-			getAllFriend,
-			addFriend,
-			searchUserByName,
-			getRequestList,
-			agreeFriend,
-			getFriendById,
-		};
-	},
-	{
-		persist: {
-			storage: localStorage,
-		},
-	}
+    return {
+      friendList,
+      getAllFriend,
+      addFriend,
+      searchUserByName,
+      getRequestList,
+      agreeFriend,
+      getFriendById,
+    };
+  },
+  {
+    persist: {
+      storage: localStorage,
+    },
+  }
 );
