22bbb5a68fc28d675a7043ed2f0e341348250686
zenos <zenos.chen@foxmail.com>
2025-04-08 21:06:23 +0800

    在初始化的时候，获取群聊列表和好友列表

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,27 @@
 import { useUserStore } from "./store/useUserStore";
 import { setThemeColor } from "./util/setTheme";
 import { useSocketStore } from "./store/useSocketStore";
+import { useFriendStore } from "./store/useFriendStore";
+import { useGroupStore } from "./store/userGroupStore";
 const userStore = useUserStore();
+const friendStore = useFriendStore();
+const groupStore = useGroupStore();
 const socketStore = useSocketStore();
 onMounted(async () => {
-  setThemeColor("#08bf61", "#129611");
-  // app initial
-  await userStore.getUserInfo();
-  socketStore.socketInitial();
+	setThemeColor("#08bf61", "#129611");
+	// app initial
+	await userStore.getUserInfo();
+	friendStore.getBlackList(); // 获取拉黑列表
+	friendStore.getAllFriend(); // 请求好友列表
+	groupStore.getGroupChatList(); // 请求群聊列表
+	socketStore.socketInitial();
 });
 </script>
 
 <template>
-  <div>
-    <RouterView></RouterView>
-  </div>
+	<div>
+		<RouterView></RouterView>
+	</div>
 </template>
 
 <style scoped></style>
