14eedfe6125c92b95e025aeaf0c7b11d404e91ed
zenos <zenos.chen@foxmail.com>
2025-04-19 12:43:25 +0800

    test git push

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-import { useUserStore } from "./store/useUserStore";
-import { setThemeColor } from "./util/setTheme";
-import { useSocketStore } from "./store/useSocketStore";
-import { useFriendStore } from "./store/useFriendStore";
-import { useGroupStore } from "./store/userGroupStore";
-const userStore = useUserStore();
-const friendStore = useFriendStore();
-const groupStore = useGroupStore();
-const socketStore = useSocketStore();
-onMounted(async () => {
-	setThemeColor("#08bf61", "#129611");
-	// app initial
-	await userStore.getUserInfo();
-	friendStore.getBlackList(); // 获取拉黑列表
-	friendStore.getAllFriend(); // 请求好友列表
-	groupStore.getGroupChatList(); // 请求群聊列表
-	socketStore.socketInitial();
-});
+	import { useUserStore } from "./store/useUserStore";
+	import { setThemeColor } from "./util/setTheme";
+	import { useSocketStore } from "./store/useSocketStore";
+	import { useFriendStore } from "./store/useFriendStore";
+	import { useGroupStore } from "./store/userGroupStore";
+	const userStore = useUserStore();
+	const friendStore = useFriendStore();
+	const groupStore = useGroupStore();
+	const socketStore = useSocketStore();
+	onMounted(async () => {
+		setThemeColor("#08bf61", "#129611");
+		// app initial
+		await userStore.getUserInfo();
+		friendStore.getBlackList(); // 获取拉黑列表
+		friendStore.getAllFriend(); // 请求好友列表
+		groupStore.getGroupChatList(); // 请求群聊列表
+		socketStore.socketInitial();
+
+		// test git push
+	});
 </script>
 
 <template>
