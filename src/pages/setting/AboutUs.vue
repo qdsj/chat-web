d93a338ec6243824c9d7b1c81c6beb485681c961
zenos <zenos.chen@foxmail.com>
2025-04-26 22:44:25 +0800

    update code

--- src/pages/setting/AboutUs.vue
+++ src/pages/setting/AboutUs.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import config from "../../../package.json";
+  import config from "../../../package.json";
 
-// TODO 检查更新
-const checkUpdate = () => {};
+  // TODO: 检查更新
+  const checkUpdate = () => { };
 </script>
 
 <template>
   <ContentPanel>
-    <el-form ref="formDataRef" label-width="180px">
+    <el-form ref="formDataRef"
+             label-width="180px">
       <el-form-item label="版本信息">
         <div class="verson-info">
           <div>版本 {{ config.version }}</div>
           <div>
-            <el-button type="primary" @click="checkUpdate">检查更新</el-button>
+            <el-button type="primary"
+                       @click="checkUpdate">检查更新</el-button>
           </div>
         </div>
       </el-form-item>
--- src/pages/setting/UserInfo.vue
+++ src/pages/setting/UserInfo.vue
@@ -1,52 +1,50 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
-import UserInfoEdit from "./UserInfoEdit.vue";
-import UserInfoPassword from "./UserInfoPassword.vue";
-import { useUserStore } from "@/store/useUserStore";
+  import { useRouter } from "vue-router";
+  import UserInfoEdit from "./UserInfoEdit.vue";
+  import UserInfoPassword from "./UserInfoPassword.vue";
+  import { useUserStore } from "@/store/useUserStore";
 
-const userStore = useUserStore();
-const router = useRouter();
+  const userStore = useUserStore();
+  const router = useRouter();
 
-const showType = ref(0);
-const changePart = (part: number) => {
-  showType.value = part;
-};
-// 退出登录
-const logout = async () => {
-  localStorage.removeItem("auth-token");
-  localStorage.removeItem("refresh-token");
-  localStorage.removeItem("user-info-store");
-  localStorage.removeItem("chat");
-  router.push("/chat");
-  await userStore.getUserInfo();
-};
+  const showType = ref(0);
+  const changePart = (part: number) => {
+    showType.value = part;
+  };
+  // 退出登录
+  const logout = async () => {
+    localStorage.removeItem("auth-token");
+    localStorage.removeItem("refresh-token");
+    localStorage.removeItem("user-info-store");
+    localStorage.removeItem("chat");
+    router.push("/chat");
+    await userStore.getUserInfo();
+  };
 
-// 编辑返回
-const editBack = () => {
-  showType.value = 0;
-};
+  // 编辑返回
+  const editBack = () => {
+    showType.value = 0;
+  };
 </script>
 
 <template>
   <ContentPanel>
     <!-- 个人信息详情 -->
-    <div class="show-info" v-if="showType == 0">
+    <div class="show-info"
+         v-if="showType == 0">
       <div class="user-info">
-        <!-- TODO 个人用户信息-->
+        <!-- TODO: 个人用户信息-->
         <UserBaseInfo :userInfo="userStore.userInfo!"></UserBaseInfo>
         <div class="more-op">
-          <el-dropdown placement="bottom-end" trigger="click">
+          <el-dropdown placement="bottom-end"
+                       trigger="click">
             <span class="el-dropdown-link">
               <div class="iconfont icon-icon_more"></div>
             </span>
             <template #dropdown>
               <el-dropdown-menu>
-                <el-dropdown-item @click="changePart(1)"
-                  >修改个人信息</el-dropdown-item
-                >
-                <el-dropdown-item @click="changePart(2)"
-                  >修改密码</el-dropdown-item
-                >
+                <el-dropdown-item @click="changePart(1)">修改个人信息</el-dropdown-item>
+                <el-dropdown-item @click="changePart(2)">修改密码</el-dropdown-item>
               </el-dropdown-menu>
             </template>
           </el-dropdown>
@@ -65,10 +63,8 @@ const editBack = () => {
 
     <!-- 修改个人信息 -->
     <div v-if="showType == 1">
-      <UserInfoEdit
-        :data="userStore.userInfo!"
-        @editBack="editBack"
-      ></UserInfoEdit>
+      <UserInfoEdit :data="userStore.userInfo!"
+                    @editBack="editBack"></UserInfoEdit>
     </div>
 
     <!-- 修改密码 -->
@@ -79,43 +75,51 @@ const editBack = () => {
 </template>
 
 <style scoped lang="scss">
-.show-info {
-  .user-info {
-    position: relative;
-    .more-op {
-      position: absolute;
-      right: 0px;
-      top: 20px;
-      .el-dropdown-link {
-        .iconfont {
-          font-size: 20px;
-        }
-        .icon-icon_more {
-          color: #9e9e9e;
-          &:hover {
-            background: #ddd;
+  .show-info {
+    .user-info {
+      position: relative;
+
+      .more-op {
+        position: absolute;
+        right: 0px;
+        top: 20px;
+
+        .el-dropdown-link {
+          .iconfont {
+            font-size: 20px;
+          }
+
+          .icon-icon_more {
+            color: #9e9e9e;
+
+            &:hover {
+              background: #ddd;
+            }
           }
         }
       }
     }
-  }
-  .part-item {
-    display: flex;
-    border-bottom: 1px solid #eaeaea;
-    padding: 20px 0px;
-    .paer-title {
-      width: 60px;
-      color: #9e9e9e;
+
+    .part-item {
+      display: flex;
+      border-bottom: 1px solid #eaeaea;
+      padding: 20px 0px;
+
+      .paer-title {
+        width: 60px;
+        color: #9e9e9e;
+      }
+
+      .part-content {
+        flex: 1;
+        margin-left: 15px;
+        color: #161616;
+      }
     }
-    .part-content {
-      flex: 1;
-      margin-left: 15px;
-      color: #161616;
+
+    .logout {
+      text-align: center;
+      margin-top: 20px;
     }
   }
-  .logout {
-    text-align: center;
-    margin-top: 20px;
-  }
-}
 </style>
